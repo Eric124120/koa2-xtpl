d23eaba3badae255d9d7a7abd238fc7b408a9aed
huangchengwen <17710053465@163.com>
2017-07-28 17:26:25 +0800

    add api_error formatter

--- utils/response_formatter.js
+++ utils/response_formatter.js
@@ -1,10 +1,9 @@
 /**
  * Created by huangchengwen on 2017/7/27.
  */
-var response_formatter = async (ctx, next) => {
-    //先去执行路由
-    await next();
+var ApiError = require('./api_error');
 
+var response_formatter = (ctx) => {
     //如果有返回数据，将返回数据添加到data中
     if (ctx.body) {
         ctx.body = {
@@ -20,4 +19,33 @@ var response_formatter = async (ctx, next) => {
     }
 }
 
-module.exports = response_formatter;
+var url_filter = function(pattern){
+
+    return async function(ctx, next){
+
+        var reg = new RegExp(pattern);
+
+        // try catch 捕获异常
+        try {
+            //先去执行路由
+            await next();
+        } catch (error) {
+            //如果异常类型是API异常并且通过正则验证的url，将错误信息添加到响应体中返回。
+            if(error instanceof ApiError && reg.test(ctx.originalUrl)){
+                ctx.status = 200;
+                ctx.body = {
+                    code: error.code,
+                    message: error.message
+                }
+            }
+            //继续抛，让外层中间件处理日志
+            throw error;
+        }
+
+        //通过正则的url进行格式化处理
+        if(reg.test(ctx.originalUrl)){
+            response_formatter(ctx);
+        }
+    }
+}
+module.exports = url_filter;
